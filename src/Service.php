fddc5629e965ea61715dd82787a3204fc54090e9
xielei <66499598+xielei@users.noreply.github.com>
2021-06-17 09:34:22 +0800

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -9,7 +9,7 @@ use Swoole\Coroutine;
 use Swoole\Server as SwooleServer;
 use Xielei\Swoole\Library\Globals;
 
-define('SW_VERSION', '1.0.0');
+define('SW_VERSION', '1.0.2');
 
 /**
  * @property Globals $globals
