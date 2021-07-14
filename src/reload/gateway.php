c78566bc19ae8ad4bb4eca2e7804dd486f7ee4cb
xielei <66499598+xielei@users.noreply.github.com>
2021-07-14 11:25:15 +0800

    Update gateway.php

--- src/reload/gateway.php
+++ src/reload/gateway.php
@@ -1,17 +1,87 @@
 <?php
 
 use Swoole\Server;
+use Swoole\Timer;
 use Xielei\Swoole\Worker;
 
 /**
  * @var Worker $this
  */
 
-foreach (['Connect', 'Receive', 'Close', 'Open', 'Message'] as $event) {
-    $this->on($event, function (Server $server, ...$args) use ($event) {
+$this->on('Connect', function (Server $server, ...$args) {
+
+    $fd = $args[0]->fd;
+    $this->throttle_fd_list[$fd] = [
+        'timer' => Timer::tick($this->throttle_interval, function () use ($fd) {
+            $this->throttle_fd_list[$fd]['times'] = $this->throttle_times;
+        }),
+        'times' => $this->throttle_times,
+    ];
+
+    $this->sendToProcess([
+        'event' => 'Connect',
+        'args' => json_decode(json_encode($args), true),
+    ]);
+});
+
+$this->on('Open', function (Server $server, ...$args) {
+
+    $fd = $args[0]->fd;
+    $this->throttle_fd_list[$fd] = [
+        'timer' => Timer::tick($this->throttle_interval, function () use ($fd) {
+            $this->throttle_fd_list[$fd]['times'] = $this->throttle_times;
+        }),
+        'times' => $this->throttle_times,
+    ];
+
+    $this->sendToProcess([
+        'event' => 'Open',
+        'args' => json_decode(json_encode($args), true),
+    ]);
+});
+
+$this->on('Receive', function (Server $server, ...$args) {
+
+    $fd = $args[0]->fd;
+    $this->throttle_fd_list[$fd]['times'] -= 1;
+    if ($this->throttle_fd_list[$fd]['times'] < 0) {
+        $server->close($fd, true);
+    } else {
         $this->sendToProcess([
-            'event' => $event,
+            'event' => 'Receive',
             'args' => json_decode(json_encode($args), true),
         ]);
-    });
-}
+    }
+});
+
+$this->on('Message', function (Server $server, ...$args) {
+
+    $fd = $args[0]->fd;
+    $this->throttle_fd_list[$fd]['times'] -= 1;
+    if ($this->throttle_fd_list[$fd]['times'] < 0) {
+        $server->close($fd, true);
+    } else {
+        $this->sendToProcess([
+            'event' => 'Message',
+            'args' => json_decode(json_encode($args), true),
+        ]);
+    }
+});
+
+$this->on('Close', function (Server $server, ...$args) {
+
+    $fd = $args[0]->fd;
+    Timer::clear($this->throttle_fd_list[$fd]['timer']);
+    unset($this->throttle_fd_list[$fd]);
+
+    $this->sendToProcess([
+        'event' => 'Close',
+        'args' => json_decode(json_encode($args), true),
+    ]);
+});
+
+$this->on('WorkerExit', function (Server $server, ...$args) {
+    foreach ($this->throttle_fd_list as $value) {
+        Timer::clear($value['timer']);
+    }
+});
