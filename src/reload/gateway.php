e2d020bf4f247fe60fd7415f210aa71bdceca996
xielei <66499598+xielei@users.noreply.github.com>
2021-07-31 09:55:23 +0800

    Update gateway.php

--- src/reload/gateway.php
+++ src/reload/gateway.php
@@ -2,81 +2,75 @@
 
 use Swoole\Server;
 use Swoole\Timer;
-use Xielei\Swoole\Worker;
+use Xielei\Swoole\Gateway;
 
 /**
- * @var Worker $this
+ * @var Gateway $this
  */
 
-$this->on('Connect', function (Server $server, ...$args) {
+foreach (['Connect', 'Open', 'Receive', 'Message'] as $event) {
+    $this->on($event, function (Server $server, ...$args) use ($event) {
 
-    $fd = $args[0]->fd;
-    $this->throttle_fd_list[$fd] = [
-        'timer' => Timer::tick($this->throttle_interval, function () use ($fd) {
-            $this->throttle_fd_list[$fd]['times'] = $this->throttle_times;
-        }),
-        'times' => $this->throttle_times,
-    ];
+        if ($this->throttle) {
+            $info = $server->getClientInfo($args[0]->fd, $args[0]->reactor_id, true);
+            if (is_array($info) && isset($info['remote_ip'])) {
+                $ip = $info['remote_ip'];
+                if (!isset($this->throttle_list[$ip])) {
+                    $this->throttle_list[$ip] = [
+                        'timer' => Timer::tick($this->throttle_interval, function () use ($ip) {
+                            if ($this->throttle_list[$ip]['fd_list']) {
+                                $this->throttle_list[$ip]['times'] = $this->throttle_times;
+                            } else {
+                                Timer::clear($this->throttle_list[$ip]['timer']);
+                                unset($this->throttle_list[$ip]);
+                            }
+                        }),
+                        'times' => $this->throttle_times,
+                        'fd_list' => [],
+                    ];
+                }
+                if (!isset($this->throttle_list[$ip]['fd_list'][$args[0]->fd])) {
+                    $this->throttle_list[$ip]['fd_list'][$args[0]->fd] = $args[0]->fd;
+                }
+                $this->throttle_list[$ip]['times'] -= 1;
+                if ($this->throttle_list[$ip]['times'] < 0) {
+                    switch ($this->throttle_close) {
+                        case 1:
+                        case 2:
+                            $server->close($args[0]->fd, $this->throttle_close == 2 ? true : null);
+                            break;
 
-    $this->sendToProcess([
-        'event' => 'Connect',
-        'args' => json_decode(json_encode($args), true),
-    ]);
-});
+                        case 3:
+                        case 4:
+                            foreach ($this->throttle_list[$ip]['fd_list'] as $value) {
+                                $server->close($value, $this->throttle_close == 4 ? true : null);
+                            }
+                            break;
 
-$this->on('Open', function (Server $server, ...$args) {
-
-    $fd = $args[0]->fd;
-    $this->throttle_fd_list[$fd] = [
-        'timer' => Timer::tick($this->throttle_interval, function () use ($fd) {
-            $this->throttle_fd_list[$fd]['times'] = $this->throttle_times;
-        }),
-        'times' => $this->throttle_times,
-    ];
-
-    $this->sendToProcess([
-        'event' => 'Open',
-        'args' => json_decode(json_encode($args), true),
-    ]);
-});
-
-$this->on('Receive', function (Server $server, ...$args) {
-
-    $fd = $args[0]->fd;
-    $this->throttle_fd_list[$fd]['times'] -= 1;
-    if ($this->throttle_fd_list[$fd]['times'] < 0) {
-        if ($this->throttle_close) {
-            $server->close($fd, $this->throttle_close == 2 ? true : null);
+                        default:
+                            break;
+                    }
+                    return;
+                }
+            }
         }
-    } else {
+
         $this->sendToProcess([
-            'event' => 'Receive',
+            'event' => $event,
             'args' => json_decode(json_encode($args), true),
         ]);
-    }
-});
-
-$this->on('Message', function (Server $server, ...$args) {
-
-    $fd = $args[0]->fd;
-    $this->throttle_fd_list[$fd]['times'] -= 1;
-    if ($this->throttle_fd_list[$fd]['times'] < 0) {
-        if ($this->throttle_close) {
-            $server->close($fd, $this->throttle_close == 2 ? true : null);
-        }
-    } else {
-        $this->sendToProcess([
-            'event' => 'Message',
-            'args' => json_decode(json_encode($args), true),
-        ]);
-    }
-});
+    });
+}
 
 $this->on('Close', function (Server $server, ...$args) {
 
-    $fd = $args[0]->fd;
-    Timer::clear($this->throttle_fd_list[$fd]['timer']);
-    unset($this->throttle_fd_list[$fd]);
+    if ($this->throttle) {
+        $info = $server->getClientInfo($args[0]->fd, $args[0]->reactor_id, true);
+        if (is_array($info) && isset($info['remote_ip'])) {
+            $ip = $info['remote_ip'];
+            unset($this->throttle_list[$ip]['fd_list'][$args[0]->fd]);
+        }
+    }
 
     $this->sendToProcess([
         'event' => 'Close',
@@ -85,7 +79,7 @@ $this->on('Close', function (Server $server, ...$args) {
 });
 
 $this->on('WorkerExit', function (Server $server, ...$args) {
-    foreach ($this->throttle_fd_list as $value) {
+    foreach ($this->throttle_list as $value) {
         Timer::clear($value['timer']);
     }
 });
