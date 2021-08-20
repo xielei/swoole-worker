ed622ee6189c641e7ba8e248e620f69955e05b1d
xielei <66499598+xielei@users.noreply.github.com>
2021-08-20 15:10:14 +0800

    Create Config.php

---
+++ src/Library/Config.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xielei\Swoole\Library;
+
+class Config
+{
+    private static $configs = [];
+
+    public static function set(string $key, $value)
+    {
+        self::$configs[$key] = $value;
+    }
+
+    public static function get(string $key, $default = null)
+    {
+        return self::$configs[$key] ?? $default;
+    }
+
+    public static function delete(string $key)
+    {
+        unset(self::$configs[$key]);
+    }
+
+    public static function isset(string $key)
+    {
+        return isset(self::$configs[$key]);
+    }
+}
